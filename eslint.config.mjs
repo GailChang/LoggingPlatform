ba27c442f59e1618808d5749e68e9c0e55a336e8
gailchang <gail863@gmail.com>
2026-06-22 22:57:05 +0800

    chore: 更新設定，及新增 prettier 設定檔

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +14,16 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn', // 使用 any 類型時僅警告
+
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off', // 只在生產環境警告 console.log
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off' // 只在生產環境警告 debugger
+    }
+  },
+  eslintConfigPrettier,
 ]);
 
 export default eslintConfig;

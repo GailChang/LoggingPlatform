a510d0f579bb746f0469c233b237cf967ce76c5f
gailchang <gail863@gmail.com>
2026-06-24 20:46:50 +0800

    feat: 按鈕顏色修改、語意化時間實作

--- src/components/LogSearchSection/LogSearchSection.tsx
+++ src/components/LogSearchSection/LogSearchSection.tsx
@@ -13,14 +13,16 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  SelectChangeEvent,
   Stack,
   TextField,
 } from '@mui/material'
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker'
+import dayjs, { Dayjs, ManipulateType } from 'dayjs'
 import 'dayjs/locale/zh-tw'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import SearchSection from '../SearchSection'
 
 //#region 常數
@@ -54,15 +56,15 @@ const TIME_OPTION = [
     label: '10分鐘以內',
   },
   {
-    value: 'tenMinutes',
+    value: 'oneHour',
     label: '1小時以內',
   },
   {
-    value: 'tenMinutes',
+    value: 'twentyfourHours',
     label: '24小時以內',
   },
   {
-    value: 'tenMinutes',
+    value: 'threeDays',
     label: '3天內',
   },
 ] as const
@@ -99,7 +101,7 @@ const STATUS_OPTION = [
   },
 ] as const
 
-//#endregion
+//#endregion 常數
 
 export default function LogSearchSection({
   defaultExpanded,
@@ -107,14 +109,109 @@ export default function LogSearchSection({
 }: Omit<AccordionProps, 'children'>) {
   const formRef = useRef<HTMLFormElement>(null)
 
+  // 管理時間欄位的狀態
+  const [stringTime, setStringTime] = useState<string>('')
+  const [startTime, setStartTime] = useState<Dayjs | null>(null)
+  const [endTime, setEndTime] = useState<Dayjs | null>(null)
+
+  //#region 時間欄位連動事件
+  /**
+   * 當「查詢期間」改變時，計算並套用起始與結束時間
+   *
+   * @param e 變動事件
+   * @returns
+   */
+  const handleStringTimeChange = (e: SelectChangeEvent<string>) => {
+    const value = e.target.value
+    setStringTime(value)
+
+    if (!value) {
+      clearAllDateTime()
+      return
+    }
+
+    const selectedOption = TIME_OPTION.find((opt) => opt.value === value)
+    if (selectedOption) {
+      const now = dayjs()
+
+      let diffNumber = 0
+      let diffUnit: ManipulateType = 'minute'
+      if (selectedOption.value === 'fiveMinutes') {
+        diffNumber = 5
+      } else if (selectedOption.value == 'tenMinutes') {
+        diffNumber = 10
+      }
+
+      if (selectedOption.value == 'oneHour') {
+        diffNumber = 1
+        diffUnit = 'hour'
+      } else if (selectedOption.value == 'twentyfourHours') {
+        diffNumber = 24
+        diffUnit = 'hour'
+      }
+
+      if (selectedOption.value == 'threeDays') {
+        diffNumber = 3
+        diffUnit = 'day'
+      }
+
+      // 如果未選取有效的值
+      if (diffNumber == 0 && diffUnit == 'minute') {
+        clearAllDateTime()
+        return
+      }
+
+      const start = now.subtract(diffNumber, diffUnit)
+
+      setEndTime(now)
+      setStartTime(start)
+    }
+  }
+
+  /** 清空查詢的起始時間、結束時間 */
+  const clearAllDateTime = () => {
+    setEndTime(null)
+    setStartTime(null)
+  }
+
+  /**
+   * 當「起始時間」或「結束時間」被手動改變時，清空「查詢期間」
+   *
+   * @param type 起始/結束
+   * @returns
+   */
+  const handleDateTimeChange =
+    (type: 'start' | 'end') => (value: Dayjs | null) => {
+      if (type === 'start') {
+        setStartTime(value)
+      } else {
+        setEndTime(value)
+      }
+      // 使用者手動調整後，將查詢期間下拉選項清空
+      setStringTime('')
+    }
+
+  //#endregion 時間欄位連動事件
+
+  //#region 按鈕事件
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
     if (!formRef.current) return
-    if (!formRef.current) return
+
     const formData = new FormData(formRef.current)
     const data = Object.fromEntries(formData.entries())
     console.log('submit data: ', data)
   }
+
+  //TODO: 待實作
+  const handleReset = () => {}
+
+  //TODO: 待實作
+  const handleSaveFilters = () => {}
+
+  //TODO: 待實作
+  const handleLoadFilters = () => {}
+  //#endregion 按鈕事件
 
   return (
     <SearchSection defaultExpanded={defaultExpanded} {...props}>
@@ -192,11 +289,20 @@ export default function LogSearchSection({
               </Select>
             </FormControl>
           </Grid>
+          {/* 查詢期間區塊 */}
           <Grid size={9}>
             <FormControl fullWidth sx={{ m: 1 }}>
               <FormLabel>查詢期間</FormLabel>
               <Stack direction='row' spacing={2} sx={{ alignItems: 'center' }}>
-                <Select name='stringTime' defaultValue='' sx={{ flex: 1 }}>
+                <Select
+                  name='stringTime'
+                  value={stringTime}
+                  defaultValue=''
+                  displayEmpty
+                  onChange={handleStringTimeChange}
+                  sx={{ flex: 1 }}
+                >
+                  <MenuItem value=''>請選擇查詢期間</MenuItem>
                   {TIME_OPTION.map((option) => (
                     <MenuItem key={option.label} value={option.value}>
                       {option.label}
@@ -210,11 +316,15 @@ export default function LogSearchSection({
                   <DateTimePicker
                     name='startTime'
                     label='起始時間'
+                    value={startTime}
+                    onChange={handleDateTimeChange('start')}
                     slotProps={{ textField: { sx: { flex: 1 } } }}
                   />
                   <DateTimePicker
                     name='endTime'
                     label='結束時間'
+                    value={endTime}
+                    onChange={handleDateTimeChange('end')}
                     slotProps={{ textField: { sx: { flex: 1 } } }}
                   />
                 </LocalizationProvider>
@@ -227,8 +337,8 @@ export default function LogSearchSection({
               <TextField
                 id='input-keyword'
                 name='keyword'
-                defaultValue=''
                 placeholder='請輸入想搜尋的文字'
+                defaultValue=''
               />
             </FormControl>
           </Grid>
@@ -248,17 +358,24 @@ export default function LogSearchSection({
               <Button
                 variant='outlined'
                 color='secondary'
+                onClick={handleReset}
                 startIcon={<RestartAltRoundedIcon />}
               >
                 重設
               </Button>
               <Button
                 variant='outlined'
+                color='cOrange'
+                onClick={handleLoadFilters}
                 startIcon={<HourglassBottomRoundedIcon />}
               >
                 讀取上次條件
               </Button>
-              <Button variant='outlined' startIcon={<SaveRoundedIcon />}>
+              <Button
+                variant='outlined'
+                onClick={handleSaveFilters}
+                startIcon={<SaveRoundedIcon />}
+              >
                 儲存條件
               </Button>
             </Stack>
--- src/style/theme.ts
+++ src/style/theme.ts
@@ -2,6 +2,24 @@
 import { grey } from '@mui/material/colors'
 import { createTheme } from '@mui/material/styles'
 
+declare module '@mui/material/styles' {
+  // 擴充 Palette 介面
+  interface Palette {
+    cOrange: Palette['primary']
+  }
+
+  // 擴充 PaletteOptions 介面（讓 createTheme 時可以傳入這些顏色）
+  interface PaletteOptions {
+    cOrange?: PaletteOptions['primary']
+  }
+}
+
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {
+    cOrange: true
+  }
+}
+
 const theme = createTheme({
   cssVariables: true,
   typography: {
@@ -13,6 +31,9 @@ const theme = createTheme({
       main: grey[700],
       dark: grey[100],
       contrastText: grey[50],
+    },
+    cOrange: {
+      main: '#d7902c',
     },
   },
 })

4cf332afbc48b4606eb4229c210b667ce4302c0d
gailchang <gail863@gmail.com>
2026-07-29 18:36:28 +0800

    feat: bottomDrawer 放入 log 頁面

--- src/app/(main)/logs/page.tsx
+++ src/app/(main)/logs/page.tsx
@@ -1,24 +1,42 @@
 'use client'
 
+import BottomDrawer from '@/components/BottomDrawer'
 import LogDataLayer from '@/components/LogDataLayer'
 import LogSearchSection from '@/components/LogSearchSection'
 import LogViewer from '@/components/LogViewer'
 import { Box, Container, Typography } from '@mui/material'
+import { useCallback, useRef, useState } from 'react'
 
 export default function LogsPage() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+  const containerRef = useRef<HTMLDivElement | null>(null)
+  const getContainer = useCallback(() => containerRef.current, [])
+
   return (
-    <Container maxWidth='xl'>
-      <Box sx={{ py: 3 }}>
-        <Typography component='h1' variant='h4' gutterBottom>
-          Logs
-        </Typography>
-        <Box sx={{ mt: 3 }}>
-          <LogSearchSection />
-          <LogDataLayer>
-            <LogViewer />
-          </LogDataLayer>
+    <>
+      <Container
+        id='conrel'
+        ref={containerRef}
+        maxWidth='xl'
+        sx={{ position: 'relative', overflow: 'hidden' }}
+      >
+        <Box id='logPage' sx={{ py: 3 }}>
+          <Typography component='h1' variant='h4' gutterBottom>
+            Logs
+          </Typography>
+          <Box sx={{ mt: 3 }}>
+            <LogSearchSection />
+            <LogDataLayer>
+              <LogViewer toggleOpen={setIsDrawerOpen} />
+            </LogDataLayer>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+      <BottomDrawer
+        container={getContainer}
+        open={isDrawerOpen}
+        toggleOpen={setIsDrawerOpen}
+      />
+    </>
   )
 }

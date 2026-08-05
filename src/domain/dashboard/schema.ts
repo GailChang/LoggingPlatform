dfc9236acece6b6a7c4484327c717447a16f970f
gailchang <gail863@gmail.com>
2026-08-05 20:42:22 +0800

    feat: 初步擬定 dashboard 圖說 schema

---
+++ src/domain/dashboard/schema.ts
@@ -0,0 +1,36 @@
+import { FilterGroup } from '../filter/schema'
+
+/** 時間軸圖 */
+export type TTimeline = {
+  createTime: string | undefined
+  logCount: number | undefined
+}
+
+/** 熱點圖-單位 schema */
+export type THeatMap = {
+  hour: number | undefined
+  logCount: number | undefined
+}
+
+/** 熱點圖資料集 */
+export type THeatMapDataset = {
+  days3ago: THeatMap[] | undefined
+  days2ago: THeatMap[] | undefined
+  days1ago: THeatMap[] | undefined
+  today: THeatMap[] | undefined
+}
+
+/** 重複錯誤圖-單位 schema */
+export type TDuplicateError = {
+  date: string | undefined
+  errorCode: string | undefined
+  path: string | undefined
+  filterGroup: FilterGroup
+  logCount: number | undefined
+}
+
+/** 重複錯誤圖資料集 */
+export type TDuplicateErrorDataset = {
+  days: TDuplicateError[] | undefined
+  week: TDuplicateError[] | undefined
+}
